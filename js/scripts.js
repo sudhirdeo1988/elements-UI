3f198dddd8fd15842f1cf18748bb086f6b53648c
sudhirdeo1988 <sudhir.deo1988@gmail.com>
2020-04-29 18:46:40 +0530

    my account, faq

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,30 @@
 $(function() {
 
-  $(".owl-carousel").owlCarousel({
-    items:1,
-    nav: true,
-    dots: true,
-    dotsEach: true
-  });
+  if($(".owl-carousel").length){
+    $(".owl-carousel").owlCarousel({
+      items:1,
+      nav: true,
+      dots: true,
+      dotsEach: true
+    });
+  }
 
-  $('.hambMobile button').on('click', function(){
+
+  $('body').delegate('.hambMobile > button','click', function(){
     var parentHead = $(this).parents('.c-header');
     if(parentHead.hasClass('st-openNav')){
       parentHead.removeClass('st-openNav');
     }else{
       parentHead.addClass('st-openNav');
+    }
+  });
+
+  $('body').delegate('.c-subNav .link','click', function(){
+    var parentHead = $(this).parents('.c-subNav');
+    if(parentHead.hasClass('st-openSubnav')){
+      parentHead.removeClass('st-openSubnav');
+    }else{
+      parentHead.addClass('st-openSubnav');
     }
   });
 
